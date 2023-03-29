a2079daca745a2c5f19b7d5fcc75c622a243dc3f
Anis <anisboudaren2@gmail.com>
2023-03-29 17:08:46 +0100

    methodes to user model

--- sahatCOM/controllers/user.controller.js
+++ sahatCOM/controllers/user.controller.js
@@ -1,39 +1,39 @@
-const createError = require('http-errors')
-const User = require('../models/user.model')
+const createError = require("http-errors");
+const User = require("../models/user.model");
 
-const addNewUser = async (req , res , next) =>{
-    const newUser = new User(req.body)
+const addNewUser = async (req, res, next) => {
+  console.log(req.body);
+  const newUser = new User(req.body);
 
-    const {error} = newUser.validation(req.body)
+  const { error } = newUser.validation(req.body);
 
-    if(error) {
-        return res.status(400).json({
-            result : false , 
-            message : error ,
+  if (error) {
+    return res.status(400).json({
+      result: false,
+      message: error,
+    });
+  } else {
+    try {
+      const result = await newUser.save();
+      if (result) {
+        res.status(201).json({
+          result: true,
+          message: "new user has been added",
         });
-
-    }else {
-        try {
-            const result = await newUser.save()
-                if(result) {
-                    res.status(201).json({
-                        result : true ,
-                        message : 'new user has been added'
-                    })
-                }else {
-                    res.status(400).json({
-                        result : false , 
-                        message : "something went wrong while saving the user"
-                    })
-                }
-        } catch (e) {
-           next(createError(e))
-        }
+      } else {
+        res.status(400).json({
+          result: false,
+          message: "something went wrong while saving the user",
+        });
+      }
+    } catch (e) {
+      next(createError(e));
     }
-}
+  }
 
-
+  next();
+};
 
 module.exports = {
-    addNewUser
-}
+  addNewUser,
+};
--- sahatCOM/models/user.model.js
+++ sahatCOM/models/user.model.js
@@ -1,46 +1,77 @@
-const Joi = require('joi');
-const mongoose = require('mongoose');
+const Joi = require("joi-oid");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      refPath: "role",
+      unique: true,
+    },
+    role: {
+      type: String,
+      enum: ["caregiver", "patient", "admin"],
+      default: "caregiver",
+    },
   },
-  password: {
-    type: String,
-    required: true
-  },
-  token: {
-    type: String,
-    required: true
-  },
-  owner : {
-    type : mongoose.Types.ObjectId , 
-    required : true , 
-    refPath : 'role'
-  } , 
-  role: {
-    type: String,
-    enum: ['caregiver', 'patient' , 'admin'],
-    default: 'caregiver'
+  { timestamps: true }
+);
+
+// used while encrypting user entered password
+userSchema.pre("save", async function () {
+  var user = this;
+  if (!user.isModified("password")) {
+    return;
+  }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(user.password, salt);
+    user.password = hash;
+  } catch (err) {
+    throw err;
   }
 });
 
+const schema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().required(),
+  token: Joi.string().required(),
+  owner: Joi.objectId().required(),
+  role: Joi.string().valid("caregiver", "admin", "patient"),
+});
 
-
-  const schema = Joi.object({
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    token: Joi.string().required(),
-    role: Joi.string().valid('caregiver', 'admin' , 'patient')
-  });
- 
 userSchema.methods.validation = function (userObject) {
-    schema.required();
-    return schema.validate(userObject);
+  schema.required();
+  return schema.validate(userObject);
 };
 
-const User = mongoose.model('User', userSchema);
+//used while signIn decrypt
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    console.log("----------------no password", this.password);
+    // @ts-ignore
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (error) {
+    throw error;
+  }
+};
 
-module.exports = User
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
